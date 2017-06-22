891200f5a8502cbee0a74d53fa634a1d5893d074
Taylor A. Liss <tayloraliss@gmail.com>
2017-06-21 21:09:55 -0400

    First commit

---
+++ src/main/java/com/taylorliss/Controller/StudentController.java
@@ -0,0 +1,23 @@
+package com.taylorliss.Controller;
+
+import com.taylorliss.Entity.Student;
+import com.taylorliss.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+}
---
+++ src/main/java/com/taylorliss/Service/StudentService.java
@@ -0,0 +1,20 @@
+package com.taylorliss.Service;
+
+import com.taylorliss.Dao.StudentDao;
+import com.taylorliss.Entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    public Collection<Student> getAllStudents(){
+        return studentDao.getAllStudents();
+    }
+
+}
